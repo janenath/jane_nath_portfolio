a7b4400058cb8bb2b79309c81d01c56fd8bd88bb
Jane <jane.e.nath@gmail.com>
2020-04-21 12:46:11 -0700

    style form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,18 +8,23 @@ const Contact = () => (
     <SEO title="Contact" />
     <form name="contact" netlify>
       <h3>let's get in touch</h3>
-      <div id="name">
-        <label>name </label>
-        <input type="text" id="name" name="name" />
-      </div>
-      <div id="email">
-        <label for="email">email address</label>
-        <input type="email" id="email" name="email" />
-      </div>
-      <div id="message">
-        <label for="message">message</label>
-        <textarea id="message" name="message"/>
-      </div>
+      <div className="formInputs">
+        <div id="name">
+          <label>name </label>
+          <br/>
+          <input type="text" id="name" name="name"  placeholder="name"/>
+        </div>
+        <div id="email">
+          <label for="email">email</label>
+          <br/>
+          <input type="email" id="email" name="email" placeholder="email"/>
+        </div>
+        <div id="message">
+          <label for="message">message</label>
+          <br/>
+          <textarea id="message" name="message" placeholder="message"/>
+        </div>
+      </div>   
       <div className="button">
         <button type="submit">send</button>
       </div>
