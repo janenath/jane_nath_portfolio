2bef79c82eda0de3d42d0e3b8e78162c98607952
Jane <jane.e.nath@gmail.com>
2020-04-21 14:20:10 -0700

    add hidden input for form submission

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,6 +7,7 @@ const Contact = () => (
   <div className="section" id="contact">
     <SEO title="Contact" />
     <form name="contact" method="POST" data-netlify="true">
+    <input type="hidden" name="form-name" value="contact" />
       <h3>let's get in touch</h3>
       <div className="formInputs">
         <div id="name">
