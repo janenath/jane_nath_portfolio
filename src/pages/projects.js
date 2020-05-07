e1bb79f12a34edc9e7c839e58b95ba233a2f74a5
Jane <jane.e.nath@gmail.com>
2020-05-07 12:45:36 -0700

    improve mobile responsiveness for project section

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -9,33 +9,29 @@ import em from "../images/em.png"
 
 const Projects = () => (
   <div className="projects" id="projects">
-    <h2>Selected Projects</h2>
+    <h2>selected projects:</h2>
     <div className="projectsGrid">
       <div className="project one">
         <a href="https://github.com/janenath/fourth_trimester" target="_blank">
           <h5><span>Fourth Trimester</span> | Full-Stack App with React, Ruby on Rails</h5>
-          <br/>
           <img src={fourth_trimester} alt="fourth trimester logo"></img>
         </a>
       </div>
       <div className="project two">
         <a href="https://github.com/janenath/capsule" target="_blank">
           <h5><span>Capsule</span> | RESTful API with React, Ruby on Rails</h5>
-          <br/>
           <img src={capsule} alt="capsule logo"></img>
         </a>
       </div>
       <div className="project three">
         <a href="https://github.com/janenath/plot-master" target="_blank">
           <h5><span>Plot Master</span> | Full-Stack App with React, Node.js</h5>
-          <br/>
           <img src={plot_master} alt="plot master logo"></img>
         </a>
       </div>
       <div className="project four">
         <a href="https://github.com/janenath/elias_miller" target="_blank">
           <h5><span>EM Portfolio</span> | Jamstack App with Gatsby, Contentful</h5>
-          <br/>
           <img src={em} alt="elias miller portfolio"></img>
         </a>
       </div>
